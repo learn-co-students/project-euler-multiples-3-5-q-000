42bae903cfb4792aa0585a200f3b69bee896c3a5
Ian Miller <irmiller22@gmail.com>
2014-10-21 14:10:43 -0400

    added solution

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,7 @@
-# Enter your procedural solution here!
+def collect_multiples(limit)
+  (1...limit).to_a.reject {|num| num unless num % 3 == 0 || num % 5 == 0}
+end
+
+def sum_multiples(limit)
+  collect_multiples(limit).inject(:+)
+end
