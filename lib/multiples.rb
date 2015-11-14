787971b200aaec4636cb08ab875dfbd7de3eed1e
cocacolabe <cocacolabear.la@gmail.com>
2015-11-13 20:59:06 -0500

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,17 @@
 # Enter your procedural solution here!
+
+
+def collect_multiples(limit)
+  collect_multiples=0
+(1...limit).select do |i|
+  if i % 3 ==0 || i % 5 == 0
+  i += collect_multiples
+end
+end
+end
+
+def sum_multiples(limit)
+ 
+  collect_multiples(limit).inject(:+)
+  
+end
