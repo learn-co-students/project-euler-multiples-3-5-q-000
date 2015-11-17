488f33906fdd762b1af6359802fd1b5984715688
Karuna <imkaruna@gmail.com>
2015-11-17 16:15:37 +0000

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,21 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+  multiples=Array.new
+  counter = 1
+  while counter < limit
+    if counter%3 == 0 || counter%5 == 0
+    multiples << counter
+  end
+    counter +=1
+  end
+  return multiples
+end
+
+def sum_multiples(limit)
+  sum = 0
+  sum_of_multiples = []
+  sum_of_multiples = collect_multiples(limit)
+  sum_of_multiples.each { |element| sum += element}
+  return sum 
+
+end
