b947703bf22dbe8e32e387398be775fd20854537
ed-karabinus <ejkarabin@gmail.com>
2015-11-19 11:56:53 -0600

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,26 @@
 # Enter your object-oriented solution here!
+class Multiples
+  def initialize(parameter)
+    @parameter = parameter
+  end
+
+  def collect_multiples
+    index = 1
+    multiples = Array.new
+    until index == @parameter
+      if index % 3 == 0 || index % 5 == 0
+        multiples << index
+      end
+      index += 1
+    end
+    multiples
+  end
+
+  def sum_multiples
+    sum = 0
+    collect_multiples.each do |multiple|
+      sum += multiple
+    end
+    sum
+  end
+end
