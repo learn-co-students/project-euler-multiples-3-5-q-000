62906508f10725fefc2d94a31614bc7e53c36f25
retraido <enzosarin@gmail.com>
2015-11-22 21:22:14 +0000

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,37 @@
-# Enter your object-oriented solution here!
+class Multiples
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def limit=(limit)
+      @limit = limit
+  end
+
+  def limit
+      @limit
+  end
+
+  def collect_multiples
+  items = (1..@limit-1).to_a
+  new_items = Array.new
+  items.each do |count|
+    if count % 3 == 0 || count % 5 == 0
+    then new_items<<count
+end
+end
+return new_items
+end
+
+def sum_multiples
+ items = (1..@limit-1).to_a
+  new_items = Array.new
+  items.each do |count|
+    if count % 3 == 0 || count % 5 == 0
+    then new_items<<count
+end
+end
+new_items.inject(:+)
+end
+
+end
