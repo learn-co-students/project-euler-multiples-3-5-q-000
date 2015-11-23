f1fd08f92e0e3d65447f20689574efef936df508
Shanetou <shanemcquerter@gmail.com>
2015-11-22 22:01:14 -0800

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,18 @@
-# Enter your object-oriented solution here!
+class Multiples
+  attr_reader :limit, :multiples
+
+  def initialize(limit)
+    @limit = limit 
+    @multiples = collect_multiples
+  end
+
+  def collect_multiples
+    (1...limit).select {|num| num % 3 == 0 || num % 5 == 0}
+  end
+
+  def sum_multiples
+    multiples.inject do |sum, num|
+      sum += num
+    end
+  end
+end
