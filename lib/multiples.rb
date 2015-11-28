84396c34defe84c52b077bcd2b03e126f2cec439
MitulMistry <mitul.mistry1@gmail.com>
2015-11-28 21:47:55 +0000

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,27 @@
 # Enter your procedural solution here!
+
+
+def collect_multiples(num)
+  array = []
+  num -= 1
+
+  (1..num).each do |i|
+    if i % 3 == 0 || i % 5 == 0
+      array << i
+    end
+  end
+
+  return array
+end
+
+def sum_multiples(num)
+  array = collect_multiples(num)
+
+  total = 0
+  array.each do |i|
+    total += i
+  end
+
+  return total
+end
+
