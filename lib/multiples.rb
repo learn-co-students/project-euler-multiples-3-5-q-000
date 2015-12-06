ea16139784b07b8226f1ffc385a308113b799957
lizdenhup <lizdenhup@gmail.com>
2015-12-05 21:08:47 -0600

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,15 @@
 # Enter your procedural solution here!
+# this method is supposed to sum all of the multiples of 3 and 5 under 1000
+def collect_multiples(limit)
+  (1...limit).find_all { |i| i % 3 == 0 || i % 5 == 0}
+end
+
+def sum_multiples(limit)
+  sum = 0
+  collect_multiples(limit).each do |i|
+    sum += i
+  end 
+  sum 
+end
+
+
