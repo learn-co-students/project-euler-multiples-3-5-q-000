3e5d4b616a1ee90c40523d56c6390e347216bb48
Rafael A. Mejia <rafael@dataminded.net>
2015-12-08 04:26:07 +0000

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,16 @@
 # Enter your procedural solution here!
+
+def collect_multiples(limit)
+  multiples = Array.new
+  (1..limit-1).each do |i|
+    if (i % 3 == 0) || (i % 5 == 0)
+      multiples.push(i)
+    end
+  end
+  return multiples
+end
+
+def sum_multiples(limit)
+  multiples = collect_multiples(limit)
+  multiples.reduce(:+)
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,25 @@
 # Enter your object-oriented solution here!
+
+class Multiples
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    multiples = Array.new
+    (1..@limit-1).each do |i|
+      if (i % 3 == 0) || (i % 5 == 0)
+        multiples.push(i)
+      end
+    end
+    return multiples
+  end
+
+  def sum_multiples
+    multiples = collect_multiples
+    multiples.reduce(:+)
+  end
+
+
+end
