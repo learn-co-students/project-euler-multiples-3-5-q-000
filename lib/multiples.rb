7baef879d5f78db3ef376b13560dda6c5fcd2df8
Eric An <erican@me.com>
2015-12-10 09:10:41 -0800

    complete Euler - multiples of 3 and 5

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,18 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+	collection = []
+  (1...limit).each do |i|
+	  if i % 3 == 0 || i % 5 == 0
+	 	  collection.push(i)
+	 	end
+  end
+  collection                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+end
+
+def sum_multiples(limit)
+	total = 0
+	collect_multiples(limit).each do |i|
+	  total += i
+	end
+	total
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,26 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+  def initialize(limit)
+  	@limit = limit
+  end
+
+	def collect_multiples
+		collection = []
+	  (1...@limit).each do |i|
+		  if i % 3 == 0 || i % 5 == 0
+		 	  collection.push(i)
+		 	end
+	  end
+	  collection                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+	end
+
+	def sum_multiples
+		total = 0
+		collect_multiples.each do |i|
+		  total += i
+		end
+		total
+	end
+
+end
