eee97973bfc867fc44a648cdfec93fbebadfb759
Juan Ruiz <juankboards@hotmail.com>
2015-12-16 02:02:08 +0000

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,15 @@
-# Enter your procedural solution here!
+def collect_multiples(range)
+  multiples = []
+  (1...range).each do |number|
+    if number%3==0 || number%5==0
+      multiples << number
+    end
+  end
+  multiples
+end
+
+def sum_multiples(range)
+  sum = 0
+  collect_multiples(range).each {|multiple| sum += multiple}
+  sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,18 @@
-# Enter your object-oriented solution here!
+class Multiples
+  def initialize (range)
+    @range = range
+  end
+
+  def  collect_multiples
+    multiples = []
+    (1...@range).select {|number| number%3==0 || number%5==0}
+  end
+
+  def sum_multiples
+    sum = 0
+    collect_multiples.each {|multiple| sum += multiple}
+    sum
+  end
+end
+
+
