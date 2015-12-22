e2cd22246b7acf63249fe60de6271c2af0df4b4c
lawrend <douglaslawrence314@gmail.com>
2015-12-21 20:10:36 -0800

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,18 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+topNum = limit - 1
+threeReps = (topNum/3)
+fiveReps = (topNum/5)
+
+threeSet = Array.new(threeReps) { |each| each = (each +1) * 3}
+fiveSet = Array.new(fiveReps) { |each| each = (each + 1) * 5}
+
+combinedSet = threeSet | fiveSet
+return combinedSet.sort
+end
+
+def sum_multiples(limit)
+  solutionSet = collect_multiples(limit)
+  return solutionSet.inject(:+)
+end
+
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,33 @@
 # Enter your object-oriented solution here!
+
+class Multiples
+  
+  def initialize(limit)
+    @topNum = limit-1
+  end
+
+  def topNum
+    @topNum
+  end
+
+  def threeReps
+    threeReps = topNum/3
+    return Array.new(threeReps) { |each| each = (each +1) * 3}
+  end
+
+  def fiveReps
+    fiveReps = topNum/5
+    return Array.new(fiveReps) { |each| each = (each +1) *5}
+  end
+
+  def collect_multiples
+    combinedSet = threeReps | fiveReps
+    return combinedSet.sort
+  end
+
+  def sum_multiples
+    return collect_multiples.inject(:+)
+  end
+end
+
+
