bcdaa3106a398f8c346a8931d338e0382b9a538e
drywaterr24 <angelolea24@gmail.com>
2015-12-27 02:10:30 -0500

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -3,6 +3,7 @@ class Multiples
 
   def initialize(num)
     @num = num
+    @z = collect_multiples
   end
 
   def collect_multiples
@@ -16,23 +17,11 @@ class Multiples
       counter += 1
     end
 
-    @z = x.select {|x| x % 3 == 0 || x % 5 == 0}
+    x.select {|x| x % 3 == 0 || x % 5 == 0}
   end
 
   def sum_multiples
-  x = Array.new
-  counter = 1
-  y = 1
-
-  until counter == @num
-    x << y
-    y += 1
-    counter += 1
-  end
-
-  z = x.select {|x| x % 3 == 0 || x % 5 == 0}
-
-  z.inject {|sum, x| sum + x}
-end 
+    @z.inject {|sum, x| sum + x}
+  end 
 
 end
