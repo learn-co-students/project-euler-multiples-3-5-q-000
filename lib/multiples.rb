0b9107043a83cfad866bac4000beecb9fb0e9913
drywaterr24 <angelolea24@gmail.com>
2015-12-27 02:03:27 -0500

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,34 @@
 # Enter your procedural solution here!
+
+
+def collect_multiples(num)
+  x = Array.new
+  counter = 1
+  y = 1
+
+  until counter == num
+    x << y
+    y += 1
+    counter += 1
+  end
+
+  x.select {|x| x % 3 == 0 || x % 5 == 0}
+end
+
+def sum_multiples(num)
+  x = Array.new
+  counter = 1
+  y = 1
+
+  until counter == num
+    x << y
+    y += 1
+    counter += 1
+  end
+
+  z = x.select {|x| x % 3 == 0 || x % 5 == 0}
+
+  z.inject {|sum, x| sum + x}
+end
+
+  
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,38 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+  def initialize(num)
+    @num = num
+  end
+
+  def collect_multiples
+    x = Array.new
+    counter = 1
+    y = 1
+
+    until counter == @num
+      x << y
+      y += 1
+      counter += 1
+    end
+
+    @z = x.select {|x| x % 3 == 0 || x % 5 == 0}
+  end
+
+  def sum_multiples
+  x = Array.new
+  counter = 1
+  y = 1
+
+  until counter == @num
+    x << y
+    y += 1
+    counter += 1
+  end
+
+  z = x.select {|x| x % 3 == 0 || x % 5 == 0}
+
+  z.inject {|sum, x| sum + x}
+end 
+
+end
