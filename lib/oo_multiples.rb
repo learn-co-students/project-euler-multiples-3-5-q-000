20a90a1ae23a6a24a0194f072a11e1b06521dfff
justin071203 <justinmaxwell0502@gmail.com>
2016-01-03 21:49:01 -0600

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,18 @@
 # Enter your object-oriented solution here!
+class Multiples
+  attr_reader :limit
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    natural_numbers = (1..limit-1).to_a
+    mul_of_three_and_five = natural_numbers.select {|x| x % 3 == 0 || x % 5== 0 }
+  end
+
+  def sum_multiples
+    natural_numbers = (1..limit-1).to_a
+    mul_of_three_and_five = natural_numbers.select {|x| x % 3 == 0 || x % 5== 0}
+    mul_of_three_and_five.inject(0) {|r, e| r + e}
+  end
+end
