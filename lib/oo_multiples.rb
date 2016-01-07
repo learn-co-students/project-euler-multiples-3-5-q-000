b76e55d1e51cd3fac417d94b6c009269a410b0dc
parora86 <parora.cpa@outlook.com>
2016-01-07 02:51:33 +0000

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,29 @@
-# Enter your object-oriented solution here!
+class Multiples
+  def initialize(limit_int)
+    @limit = limit_int - 1
+  end
+  
+  def limit()
+    @limit
+  end
+  
+  def collect_multiples()
+    multiple = []
+    while (@limit>=3) do
+      if (@limit%3==0 || @limit%5==0)
+        multiple << @limit
+      end
+      @limit -= 1
+    end
+    return multiple.reverse
+  end
+
+  def sum_multiples()
+    sum = 0
+    multiple = collect_multiples()
+    multiple.each do |i|
+      sum += i
+    end
+    return sum
+  end
+end
