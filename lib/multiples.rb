6dc8f2ef85d48a060fd062a9cb3596faa7ac2f9d
Smulligan85 <sean.mulligan85@gmail.com>
2016-01-12 20:47:20 -0800

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,24 @@
-# Enter your procedural solution here!
+def sum_multiples(limit)
+  num_list = []
+  mult = []
+  1.upto(limit - 1) { |i| num_list << i }
+  num_list.each do |n|
+    if n % 3 == 0 || n % 5 == 0
+      mult << n
+    end
+  end
+
+  mult.reduce(:+)
+end
+
+def collect_multiples(limit)
+  num_list = []
+  mult = []
+  1.upto(limit - 1) { |i| num_list << i }
+  num_list.each do |n|
+    if n % 3 == 0 || n % 5 == 0
+      mult << n
+    end
+  end
+  mult
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,30 @@
-# Enter your object-oriented solution here!
+class Multiples
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def sum_multiples
+    num_list = []
+    mult = []
+    1.upto(@limit - 1) { |i| num_list << i }
+    num_list.each do |n|
+      if n % 3 == 0 || n % 5 == 0
+        mult << n
+      end
+    end
+    mult.reduce(:+)
+  end
+
+  def collect_multiples
+    num_list = []
+    mult = []
+    1.upto(@limit - 1) { |i| num_list << i }
+    num_list.each do |n|
+      if n % 3 == 0 || n % 5 == 0
+        mult << n
+      end
+    end
+    mult
+  end
+end
