ea1d09e7ef81a9f4971017640ca7e20603024ecb
Fizdalf <fizdalf@gmail.com>
2016-01-13 14:04:27 +0000

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,11 @@
 # Enter your procedural solution here!
+def collect_multiples(limit = 10)
+  Array(1..limit-1).select {|number|
+    number % 5 == 0 || number % 3 == 0
+    }
+
+end
+
+def sum_multiples(limit = 10)
+  collect_multiples(limit).reduce(:+)
+end
