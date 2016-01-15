7f30a9869b92e0fdaffc3a760d5aefd338c4f0ba
Masha Antropova <masha.antropova@gmail.com>
2016-01-14 20:07:49 -0500

    Add solutions

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,21 @@
 # Enter your procedural solution here!
+
+
+def collect_multiples(limit)
+	multiples = []
+	start = 3
+	multiple_one = 3
+	multiple_two = 5
+
+	while start < limit
+		if start % multiple_one == 0 || start % multiple_two == 0
+			multiples << start
+		end
+		start += 1
+	end
+	return multiples
+end
+
+def sum_multiples(limit)
+	return collect_multiples(limit).inject(:+)
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,26 @@
 # Enter your object-oriented solution here!
+
+class Multiples
+	def initialize(limit)
+		@limit = limit
+	end
+
+	def collect_multiples
+		multiples = []
+		start = 3
+		multiple_one = 3
+		multiple_two = 5
+
+		while start < @limit
+			if start % multiple_one == 0 || start % multiple_two == 0
+				multiples << start
+			end
+			start += 1
+		end
+		return multiples
+	end
+
+	def sum_multiples
+		return collect_multiples.inject(:+)
+	end
+end
