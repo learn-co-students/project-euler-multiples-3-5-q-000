658e851169efd35eeb2190234278c51c1678b201
HarlemSquirrel <wittyman37@yahoo.com>
2016-01-19 17:29:39 +0000

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,18 @@
 # Enter your procedural solution here!
+
+def collect_multiples(limit)
+  multiples = []
+  for i in 1..(limit - 1)
+    if i % 3 == 0 || i % 5 == 0
+      multiples << i
+    end
+  end
+  multiples
+end
+
+def sum_multiples(limit)
+  multiples = collect_multiples(limit)
+  sum = 0
+  multiples.each { |i| sum += i }
+  sum
+end
