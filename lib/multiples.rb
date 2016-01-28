867b83ec160da18562cabfad95367357c29832dd
jes708 <jonschwarz@nyu.edu>
2016-01-28 00:24:51 -0500

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,28 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+  result = []
+  i = 3
+  while i < limit
+    
+    if i % 3 == 0 || i % 5 == 0
+      result << i
+    end
+
+    i += 1
+  end
+
+  result
+end
+
+def sum_multiples(limit)
+  result = 0
+  array = collect_multiples(limit)
+
+  i = 0
+  while i < array.length
+    result += array[i]
+    i += 1
+  end
+
+  result
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,37 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    collection = []
+    i = 3
+    while i < @limit
+    
+      if i % 3 == 0 || i % 5 == 0
+        collection << i
+      end
+
+      i += 1
+    end
+
+    @collection = collection
+    return @collection
+  end
+
+  def sum_multiples
+    result = 0
+    array = collect_multiples
+
+    i = 0
+    while i < array.length
+      result += array[i]
+      i += 1
+    end
+
+    return result
+  end
+
+end
