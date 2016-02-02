dfbe036ca60fae5bdcd185fcc96186537b4a4217
Matt Spatola <m@t.spato.la>
2016-02-02 07:34:11 -0500

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,13 @@
-# Enter your object-oriented solution here!
+class Multiples
+  def initialize(n)
+    @n = n
+  end
+
+  def collect_multiples
+    (1...@n).select { |i| i % 3 == 0 || i % 5 == 0 }
+  end
+
+  def sum_multiples
+    collect_multiples.reduce(:+)
+  end
+end
