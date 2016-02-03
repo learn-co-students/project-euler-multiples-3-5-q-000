afcd64d453fe26458d75c9d71c1cf559a9cea027
Brian Holland <beehollander@gmail.com>
2016-02-03 20:08:32 +0000

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,14 @@
 # Enter your procedural solution here!
+
+def naturals(limit)
+  return (1..limit-1).to_a
+end
+
+def collect_multiples(limit)
+  all = naturals(limit)
+  return all.select {|a| (a%3==0 || a%5==0)}
+end
+
+def sum_multiples(limit)
+  return collect_multiples(limit).reduce(0, :+)
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,21 @@
 # Enter your object-oriented solution here!
+class Multiples
+  def initialize(limit)
+    @limit=limit
+    return sum_multiples(@limit)
+  end
+
+  #Helper Methods
+  def naturals(limit=@limit)
+    return (1..limit-1).to_a
+  end
+
+  def collect_multiples(limit=@limit)
+    all = naturals(limit)
+    return all.select {|a| (a%3==0 || a%5==0)}
+  end
+
+  def sum_multiples(limit=@limit)
+   return collect_multiples(limit).reduce(0, :+)
+  end
+end
