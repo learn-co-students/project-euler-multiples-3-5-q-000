8b3376735798c01beeb0927251f091784cd9b11d
GabrielPeri <gabriel.w.peri@gmail.com>
2016-02-08 19:16:41 +0000

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,22 @@
-# Enter your procedural solution here!
+def collect_multiples(limit)
+  multiples_3 = []
+  multiples_5 = []
+  a = 0
+  b = 0
+
+  while a < limit
+    multiples_3 << a
+    a += 3
+  end
+
+  while b < limit
+    multiples_5 << b
+    b += 5
+  end
+  arr = ((multiples_5 + multiples_3) - [0]).sort.uniq
+end
+
+
+def sum_multiples(limit)
+  collect_multiples(limit).inject(0, :+)
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,28 @@
-# Enter your object-oriented solution here!
+class Multiples
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    multiples_3 = []
+    multiples_5 = []
+    a = 0
+    b = 0
+
+    while a < @limit
+      multiples_3 << a
+      a += 3
+    end
+
+    while b < @limit
+      multiples_5 << b
+      b += 5
+    end
+    arr = ((multiples_5 + multiples_3) - [0]).sort.uniq
+  end
+
+  
+  def sum_multiples
+    collect_multiples.inject(0, :+)
+  end
+end
