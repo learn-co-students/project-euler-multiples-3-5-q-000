373cf9d25cea00f8cc48bb61a24d79ca394e2859
KielaPeoples <kielapeoples@gmail.com>
2016-02-23 23:48:37 +0000

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,21 @@
 # Enter your procedural solution here!
+
+def collect_multiples(limit = 1000)
+  multiples_array = []
+  num = 1
+  while num < limit
+    if (num % 3 == 0 || num % 5 == 0)
+      multiples_array.push(num)
+    end
+    num += 1
+  end
+  return multiples_array
+end
+
+def sum_multiples(limit)
+  sum = 0
+  collect_multiples(limit).each do |multi|
+    sum += multi
+  end
+  return sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,26 @@
 # Enter your object-oriented solution here!
+class Multiples
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    multiples_array = []
+    num = 1
+    while num < @limit
+      if (num % 3 == 0 || num % 5 == 0)
+        multiples_array.push(num)
+      end
+      num += 1
+    end
+    return multiples_array
+  end
+
+  def sum_multiples
+    sum = 0
+    collect_multiples.each do |multi|
+      sum += multi
+    end
+    return sum
+  end
+end
