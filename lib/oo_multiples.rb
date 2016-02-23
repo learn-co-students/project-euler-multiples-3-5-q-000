c2534bcdb62796b5ffa410429256981dbf850a5f
bradleyoashburn <bradleyoashburn@gmail.com>
2016-02-22 14:41:08 -1000

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -2,10 +2,36 @@
 
 class Multiples
 
-def initialize(limit)
-  @upper_limit = limit
-end
+  def initialize(limit)
+    @upper_limit = limit
+  end
 
+  def collect_multiples
+    counter = 1
+    multiples = []
+    
+    while counter < @upper_limit
+      if counter % 3 == 0 || counter % 5 == 0
+        multiples.push(counter)
+      end
+      counter += 1
+    end
+
+    return multiples
+  end
+  
+  def sum_multiples
+    counter = 1
+    sum = 0;
+    while counter < @upper_limit
+      if counter % 3 == 0 || counter % 5 == 0
+        sum += counter
+      end
+      counter += 1
+    end
+
+    return sum
+  end
 
 end
 
