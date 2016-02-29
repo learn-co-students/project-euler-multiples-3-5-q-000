a89509b951ebb5a624538c4170449c0be6c66ade
Rawradin <edgar.rawr@gmail.com>
2016-02-29 11:36:58 -0800

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,18 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+  i = 1
+  array = []
+  while i < limit 
+    if i%3 == 0 || i%5==0
+      array << i
+    end
+    i += 1
+  end
+  array
+end
+
+def sum_multiples(limit)
+  array = collect_multiples(limit)
+  sum = array.inject(0){|sum,i| sum + i }
+  sum
+end
