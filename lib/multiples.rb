e93e90fa662bc3b9b3b38565c327b69cc68d5318
codeByScott <scott@codebyscott.io>
2016-03-01 21:20:20 +0900

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,20 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+  collection = []
+  limit.times do |num|
+    collection.push(num) if num % 3 == 0 or num % 5 == 0 and num != 0
+  end
+  p collection
+end
+
+def sum_multiples(limit)
+  sum = 0
+  
+  limit.times do |num|
+    sum += num if num % 3 == 0 or num % 5 == 0
+  end
+  sum
+end
+
+collect_multiples(1000)
+sum_multiples(1000)
