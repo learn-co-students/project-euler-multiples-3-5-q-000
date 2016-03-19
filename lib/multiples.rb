c78914c014267ab1e80a799ff01e01eb43e0cc6d
einarstensson <einar.stensson@gmail.com>
2016-03-19 19:44:01 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,15 @@
-# Enter your procedural solution here!
+def collect_multiples(limit)
+  multiples = []
+
+  1.upto(limit - 1) do |number|
+    multiples << number if number % 3 == 0 || number % 5 == 0
+  end
+
+  multiples
+end
+
+def sum_multiples(limit)
+  multiples = collect_multiples(limit)
+
+  multiples.reduce(:+)
+end
