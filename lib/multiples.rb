ff27de0161b0fee169de490c54cd5230c8934c5a
MIKE-RANDELL <michael.james.randell@gmail.com>
2016-03-22 16:28:58 +0000

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,10 +1,20 @@
 # Enter your procedural solution here!
-def collect_multiples
-(0...9).each do |i|
+
+def collect_multiples(multiples)
+  multiplesarr = []
+  (1...multiples).each do |i|
+    if i % 3 == 0 || i % 5 == 0
+      multiplesarr << i
+    end
+  end
+  return multiplesarr
+end
+
+def sum_multiples(multiplessum)
+  array = collect_multiples(multiplessum)
   sum = 0
-  if i % 3 == 0 || i % 5 == 0
+  array.each do |i|
     sum += i
   end
   return sum
-end
 end
