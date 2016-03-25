594ef6f32298b8699bffb06c089d50a5d38a0a8b
Dom-Mc <dom.mckellar@gmail.com>
2016-03-24 20:04:09 -0700

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,23 @@
 # Enter your procedural solution here!
+
+def collect_multiples(limit)
+  natural_numbers = []
+   limit.times do |num|
+     if num >= 3 && ((num % 3 == 0) || (num % 5 == 0))
+       natural_numbers << num
+     end
+   end
+   return natural_numbers
+end
+#collect_multiples(10)
+
+def sum_multiples(limit)
+  sum = collect_multiples(limit)
+  total = 0
+  sum.each do |y|
+    total += y
+  end
+  return total
+end
+
+sum_multiples(10)
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,28 @@
 # Enter your object-oriented solution here!
+
+class Multiples
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples()
+    natural_numbers = []
+     @limit.times do |num|
+       if num >= 3 && ((num % 3 == 0) || (num % 5 == 0))
+         natural_numbers << num
+       end
+     end
+     return natural_numbers
+  end
+
+  def sum_multiples
+    sum = collect_multiples()
+    total = 0
+    sum.each do |y|
+      total += y
+    end
+    return total
+  end
+
+end
