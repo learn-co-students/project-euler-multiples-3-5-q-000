4dabe1aced920845205c5940e78f209c256ee1c9
jin <jinzheng87@gmail.com>
2016-03-26 15:34:32 -0400

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,30 @@
 # Enter your object-oriented solution here!
+class Multiples
+    
+    def initialize(limit)
+        @limit = limit
+    end
+    
+    def limit
+        @limit
+    end
+    
+    def collect_multiples
+        collect = [ ]
+        (1...limit).each do |number|
+            if number % 3 == 0 || number % 5 == 0
+                collect << number
+            end
+        end
+        return collect
+    end
+    
+    def sum_multiples
+        sum = 0
+        collect_multiples.each do |number|
+            sum += number
+        end
+        return sum
+    end
+   
+end
