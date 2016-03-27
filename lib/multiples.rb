72e47a2d125eb0e60cbb78953b2f2d4605c28f9e
anorico <allan.norico@gmail.com>
2016-03-27 15:10:26 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,25 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+  i = 1
+  array=[]
+  while i < limit
+    if i % 3 == 0 || i % 5 == 0
+      array << i 
+    end
+    i += 1
+  end
+  return array
+end
+
+def sum_multiples(limit)
+  i = 0
+  sum = 0
+  array = []
+  while i < limit
+   if i % 3 == 0 || i % 5 == 0
+    sum += i
+   end
+   i += 1
+  end
+  return sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,33 @@
 # Enter your object-oriented solution here!
+class Multiples
+ def initialize(limit)
+  unless limit
+    raise ArgumentError.new("must inlcude a limit")
+  end
+  @limit = limit
+  @array = Array.new 
+ end
+
+ def collect_multiples
+   i = 1
+   while i < @limit
+     if i % 3 == 0 || i % 5 == 0
+       @array << i 
+     end
+     i += 1
+   end
+   return @array
+ end
+
+ def sum_multiples
+   i = 0
+   sum = 0
+   while i < @limit
+    if i % 3 == 0 || i % 5 == 0
+     sum += i
+    end
+    i += 1
+   end
+   return sum
+ end
+end
