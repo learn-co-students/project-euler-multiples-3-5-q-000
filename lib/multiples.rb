d6451846dc7c274351cde385f102bedd9e2ef3f8
thaik1618 <kthai1618@gmail.com>
2016-03-28 18:59:19 +0100

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,13 @@
-# Enter your procedural solution here!
+
+def collect_multiples(limit)
+  array = []
+  range = (1...limit).to_a
+  range.each do |x|
+    x % 3 == 0 || x % 5 == 0 ? array.push(x) : nil
+  end
+  array
+end
+
+def sum_multiples(limit)
+  collect_multiples(limit).inject(0, :+)
+end
