7ae6a1edcc183688d593fbc5d0183d4311f6ba37
BLK1010 <brenda1010@embarqmail.com>
2016-03-29 22:50:34 +0000

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,31 @@
 # Enter your procedural solution here!
+
+def collect_multiples(limit)
+  multiples = Array.new
+  counter = 1
+
+  while counter < limit
+    if counter % 3 == 0 || counter % 5 == 0
+      multiples.push(counter)
+    end
+    counter += 1
+  end
+
+  return multiples
+
+end
+
+def sum_multiples(limit)
+  multiples_sum = 0
+  counter = 1
+
+  while counter < limit
+    if counter % 3 == 0 || counter % 5 == 0
+      multiples_sum += counter
+    end
+    counter += 1
+  end
+
+  return multiples_sum
+
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,38 @@
 # Enter your object-oriented solution here!
+
+class Multiples
+  def initialize(limit)
+    @limit = limit
+  end
+  
+  def collect_multiples()
+    multiples = Array.new
+    counter = 1
+
+    while counter < @limit
+      if counter % 3 == 0 || counter % 5 == 0
+        multiples.push(counter)
+      end
+      counter += 1
+    end
+
+    return multiples
+
+  end
+
+  def sum_multiples()
+    multiples_sum = 0
+    counter = 1
+
+    while counter < @limit
+      if counter % 3 == 0 || counter % 5 == 0
+        multiples_sum += counter
+      end
+      counter += 1
+    end
+
+    return multiples_sum
+
+  end  
+  
+end
