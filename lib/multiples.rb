cf5499879a9d9a61caae595a4fe81056f152989e
M. Edgar Joel <mkakajoel@gmail.com>
2016-04-11 12:49:25 +0000

    done

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,13 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+  multiples = []
+  (1...limit).each { |n| multiples << n if n % 3 == 0 || n % 5 == 0 }
+  multiples
+end
+
+def sum_multiples(limit)
+  collect_multiples(limit).inject(:+)
+end
+
+
+
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,16 @@
 # Enter your object-oriented solution here!
+class Multiples
+  def initialize(limit)
+    @limit = limit
+    @multiples = []
+  end
+
+  def collect_multiples
+    (1...@limit).each { |n| @multiples << n if n % 3 == 0 || n % 5 == 0 }
+    @multiples
+  end
+
+  def sum_multiples
+    collect_multiples.inject(:+)
+  end
+end
