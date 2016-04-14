283e217b424772e17e420df5af6c978eed763776
LiaOlya <LiaOlya@ukr.net>
2016-04-14 09:31:27 +0000

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,16 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+  c = []
+  lim = (1..limit - 1).to_a
+    lim.each do |b|
+      if b % 3 == 0 || b % 5 == 0
+      c << b
+      end
+    end
+c
+end
+
+def sum_multiples(limit)
+  a = collect_multiples(limit)
+  a.reduce(:+)
+end
