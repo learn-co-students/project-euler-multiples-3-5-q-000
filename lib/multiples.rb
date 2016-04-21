ae9d586b7a6af2434f3a56906c8e02aa3dc9c001
ollieshmollie <olishmollie@gmail.com>
2016-04-21 13:22:16 -0400

    Update multiples.rb

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,48 +1,8 @@
 # Enter your procedural solution here!
-range = (1..1000).to_a
-
 def collect_multiples(limit)
-  multiples = Array.new
-  range = (1...limit).to_a
-  range.each do |num|
-    if num %3 == 0 || num %5 == 0
-      multiples << num
-    end
-  end
-  return multiples
+  (1...1000).to_a.keep_if{|num| num % 3 == 0 || num % 5 == 0}
 end
 
 def sum_multiples(limit)
-  sum = 0
-  collect_multiples(limit).each do |num|
-    sum += num
-  end
-  return sum
+  collect_multiples(limit).reduce(:+)
 end
-
-
-def multiple_of_5?(num)
-  num %5 == 0
-end
-
-def sum(array)
-  sum = 0
-  array.each do |num|
-    sum += num
-  end
-  return sum
-end
-
-def sum_mults(range)
-  multiples = Array.new
-  range.each do |num|
-    if multiple_of_3?(num) || multiple_of_5?(num)
-      multiples << num
-    end
-  end
-  return sum(multiples)
-end
-
-puts sum(range)
-
-
