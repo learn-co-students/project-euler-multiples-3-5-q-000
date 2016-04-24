13c01f963cff5535b2cca48eea983446f35e70af
rikari0813 <christiane.maico@gmail.com>
2016-04-24 08:21:44 +0000

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,19 @@
-# Enter your procedural solution here!
+def collect_multiples(limit)
+  lala = []
+  multiple = 3
+  while multiple < limit
+    if (multiple % 3) == 0 || (multiple % 5) == 0
+      lala.add multiple
+    end
+  end
+  lala
+end
+
+def sum_multiples(limit)
+  lala = collect_multiples(limit)
+  total = 0
+  lala.each {|number|
+    total = number + total
+    }
+  total
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,39 @@
-# Enter your object-oriented solution here!
+
+class Multiples
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    lala = []
+    multiple = 3
+    while multiple < @limit
+      if (multiple % 3) == 0 || (multiple % 5) == 0
+        lala.add multiple
+      end
+    end
+    lala
+  end
+
+  def sum_multiples
+    lala = collect_multiples
+    total = 0
+    lala.each {|number|
+      total = number + total
+    }
+    total
+  end
+
+
+
+
+
+end
+
+natural = 3
+
+while natural < 1000
+
+end
+
