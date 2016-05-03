06005b6f3bed15732f0b6bea30fac91fdd648916
Yechiel Kalmenson <chilikkal@gmail.com>
2016-05-03 09:28:51 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,11 @@
 # Enter your procedural solution here!
+def collect_multiples(number)
+  (1...number).select {|n| n % 3 == 0 || n % 5 == 0}
+end
+
+def sum_multiples(number)
+  number = collect_multiples(number)
+  sum = 0
+  number.each {|n| sum += n}
+  sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,14 @@
 # Enter your object-oriented solution here!
+class Multiples
+  def initialize(number)
+    @number = (1...number).to_a
+  end
+  def collect_multiples
+    @number.select {|n| n % 3 == 0 || n % 5 == 0}
+  end
+  def sum_multiples
+    sum = 0
+    collect_multiples.each {|n| sum += n}
+    sum
+  end
+end
