704438fbb3f8bd855a4e3feefed953ea76336230
alexseth1124 <alexseth1124@gmail.com>
2016-05-05 17:26:27 -0400

    Done

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,17 @@
-# Enter your object-oriented solution here!
+class Multiples
+  attr_reader :integer
+
+  def initialize(integer)
+    @integer = integer
+  end
+
+  def collect_multiples
+    (1..@integer-1).to_a.collect {|number| number if number % 3 == 0 || number % 5 == 0}.compact
+  end
+
+  def sum_multiples
+    collect_multiples.inject(0, :+)
+  end
+
+
+end
