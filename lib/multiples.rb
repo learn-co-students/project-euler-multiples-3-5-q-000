9eb08a1bfa10655681353868c02985d610f6a05b
Savannah Scott <sascott624@gmail.com>
2016-05-06 12:07:19 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,22 @@
 # Enter your procedural solution here!
+#Find the sum of all the multiples of 3 or 5 below 1000.
+#first, we need our array between 1 - 1000
+#then push multiples of 3 or 5 between 1-1000 into a new array (.select)
+#return the sum of the values in our second array
+
+def collect_multiples(limit)
+  multiples_array = []
+  counter = 1
+  while counter < limit
+    if counter%3 == 0 || counter%5 == 0
+      multiples_array << counter
+    end
+    counter += 1
+  end
+  return multiples_array
+end
+
+def sum_multiples(limit)
+  sum = 0
+  collect_multiples(limit).reduce(:+)
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,32 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+  def initialize (limit)
+    @limit = limit
+  end
+
+  def limit= (limit)
+    @limit = limit
+  end
+
+  def limit
+    @limit
+  end
+
+  def collect_multiples
+    multiples_array = []
+    counter = 1
+    while counter < @limit
+      if counter%3 == 0 || counter%5 == 0
+        multiples_array << counter
+      end
+      counter+=1
+    end
+    return multiples_array
+  end
+
+  def sum_multiples
+    sum = 0
+    collect_multiples.reduce(:+)
+  end
+end
