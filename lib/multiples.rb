4c33ace20e52d3dfff7db947b8de0e8a04e161ff
Dimix181 <dimix181@gmail.com>
2016-05-16 12:55:12 -0400

    added the proper names to the methods

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -3,34 +3,35 @@
 product_array = []
 product = 0
 multiplicand = 0 # 2nd changing number
+multiplier = 3 # This one is either 3 or 5
 
-def start(multiplicand = 0, product = 0, product_array = [])
+def start(multiplicand = 0, product = 0, product_array = [], multiplier = 3)
     multiplicand+=1
-    multiply(product, multiplicand, product_array)
+    multiply(product, multiplicand, product_array, multiplier)
   end
 
-def multiply(product, multiplicand, product_array)
-    product = 3*multiplicand
+def multiply(product, multiplicand, product_array, multiplier)
+    product = multiplier*multiplicand
     if validator(product) !=false
-      array_appender(product, product_array, multiplicand)
+      collect_multiples(product, product_array, multiplicand)
     else
-      sum(product_array)
+      sum_multiples(product_array)
     end
 end
 
-  def validator(product)
+def validator(product)
    product >= 10 ? false : true
   end
 
-        def array_appender(product, product_array, multiplicand)
-          product_array<<product
-          start(multiplicand,product,product_array)
-        end
+def collect_multiples(product, product_array, multiplicand)
+   product_array<<product
+   start(multiplicand,product,product_array)
+  end
 
-        def sum(product_array)
+ def sum_multiples(product_array)
       p product_array
-          product_array.reduce(:+) # adds all elements in the array
-        end
+      product_array.reduce(:+) # adds all elements in the array
+    end
 
 #TODO
 #currently just adds all the multiples of 3
