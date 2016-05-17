217577766e109d98c19fc0dfdfe71f10702d017c
Tal Goldfus <talgoldfus@gmail.com>
2016-05-17 19:48:43 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,16 @@
 # Enter your procedural solution here!
+
+def collect_multiples(limit)
+  multiples=[]
+
+for i in (1...limit)
+    multiples << i if i%3==0 || i%5==0
+end
+multiples
+end
+
+def sum_multiples(limit)
+sum=0
+collect_multiples(limit).each {|n| sum+=n}
+sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,22 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+def initialize (num)
+@limit=num
+end
+
+def collect_multiples
+  multiples=[]
+for i in (1...@limit)
+    multiples << i if i%3==0 || i%5==0
+end
+multiples
+end
+
+def sum_multiples
+sum=0
+collect_multiples.each {|n| sum+=n}
+sum
+end
+
+end
