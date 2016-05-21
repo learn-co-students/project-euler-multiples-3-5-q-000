0e522dce1ff52e186566757ebc1e289168dde8ec
Elvin <dimix181@gmail.com>
2016-05-21 01:22:23 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,39 +1,16 @@
 # Enter your procedural solution here!
 
-product_array = []
-product = 0
-multiplicand = 0 # 2nd changing number
-multiplier = 3 # This one is either 3 or 5
 
-def start(multiplicand = 0, product = 0, product_array = [], multiplier = 3)
-    multiplicand+=1
-    multiply(product, multiplicand, product_array, multiplier)
+def collect_multiples(limit)
+  product_array = Array.new
+
+  1.upto(limit-1) do |number|
+  product_array<<number if number % 3 ==0 || number % 5 == 0
   end
-
-def multiply(product, multiplicand, product_array, multiplier)
-    product = multiplier*multiplicand
-    if validator(product) !=false
-      collect_multiples(product, product_array, multiplicand)
-    else
-      sum_multiples(product_array)
-    end
+  product_array
 end
 
-def validator(product)
-   product >= 10 ? false : true
-  end
 
-def collect_multiples(product, product_array, multiplicand)
-   product_array<<product
-   start(multiplicand,product,product_array)
-  end
-
- def sum_multiples(product_array)
-      p product_array
-      product_array.reduce(:+) # adds all elements in the array
-    end
-
-#TODO
-#currently just adds all the multiples of 3
-#append all the multiples of 5 into the product_array
-#then add them
+def sum_multiples(limit)
+  collect_multiples(limit).reduce(:+)
+end
