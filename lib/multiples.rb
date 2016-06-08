8d0b3d9240c4beba25105337720cfe3977590e43
pajamaw <pjpeterww@gmail.com>
2016-06-08 18:59:45 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,14 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+  (1...limit).select{|x| x % 3 == 0 || x % 5 == 0 }
+end
+
+def sum_multiples(limit)
+  sum = 0 
+  multiples_array = collect_multiples(limit)
+
+  multiples_array.each do |num| 
+    sum += num
+  end
+  sum
+end
