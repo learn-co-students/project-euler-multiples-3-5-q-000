bde6f4775e34967c9dfcd7e17fde630ac4976db2
uu <giladbarkan@live.com>
2016-06-07 20:33:08 -0400

    passed tests

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,16 @@
 # Enter your procedural solution here!
+def collect_multiples(n)
+  (1..n-1).select{ |x| x if x % 3 == 0 || x % 5 == 0 }
+end
+
+def sum_multiples(n)
+  sum = 3
+  i = 4
+  while i < n
+    if i % 3 == 0 || i % 5 == 0
+      sum += i
+    end
+    i += 1
+  end
+  sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,23 @@
 # Enter your object-oriented solution here!
+class Multiples
+  def initialize(n)
+    @n = n
+  end
+  
+  def collect_multiples   
+    (1..@n-1).select{ |x| x if x % 3 == 0 || x % 5 == 0 }
+  end
+   
+  def sum_multiples
+    sum = 3
+    i = 4
+    while i < @n
+      if i % 3 == 0 || i % 5 == 0
+        sum += i
+      end
+      i += 1
+    end
+    sum
+  end
+
+end
