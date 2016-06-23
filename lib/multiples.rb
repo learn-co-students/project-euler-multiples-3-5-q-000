3869e242fb2aa8546cab22008619a04c8fe0eff0
Brian Reynolds <bri.reynolds@gmail.com>
2016-06-23 21:25:46 +0000

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,25 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+  counter = 1
+  multiples_array = []
+
+  while counter < limit
+    if counter % 3 == 0
+      multiples_array << counter
+    elsif counter % 5 == 0
+      multiples_array << counter
+    end
+
+    counter += 1
+  end
+
+  multiples_array
+end
+
+def sum_multiples(limit)
+  sum = 0
+
+  collect_multiples(limit).each { |a| sum += a }
+
+  sum
+end
