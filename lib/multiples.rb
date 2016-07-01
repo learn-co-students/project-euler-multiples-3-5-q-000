d208922bf82e569a86c43f6bbe035be4d7a19da1
Buntry <parkergriep@yahoo.com>
2016-06-30 16:46:10 -1000

    lab complete

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,16 @@
 # Enter your procedural solution here!
+def collect_multiples(lim)
+    multiples = []
+    
+    for i in 3..lim-1
+        multiples << i if i % 3 == 0 || i % 5 == 0
+    end 
+    
+    return multiples
+end
+
+
+def sum_multiples(lim)
+    multiples = collect_multiples(lim)
+    multiples.inject(0){|sum,x| sum + x }
+end
