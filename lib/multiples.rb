e6ba28c23309b65e1339f736ce93174ca47acdcf
Jason Lu <jlucpacpcu@gmail.com>
2016-07-09 00:36:53 +0000

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,12 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+  result = []
+  for num in (3..limit-1)
+      result << num if (num % 3 == 0 || num%5 == 0)
+  end
+  return result
+end
+
+def sum_multiples(limit)
+  collect_multiples(limit).inject(:+)
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,18 @@
 # Enter your object-oriented solution here!
+class Multiples
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    result = []
+    for num in (3..@limit-1)
+        result << num if (num % 3 == 0 || num%5 == 0)
+    end
+    return result
+  end
+
+  def sum_multiples
+    collect_multiples.inject(:+)
+  end
+end
