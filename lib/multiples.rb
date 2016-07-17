19b572b27c08ff0e43040eb693e6031e263bc9d8
Brian Cueto <bcueto1@jhu.edu>
2016-07-17 18:20:10 +0000

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,21 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+  counter = 1
+  multiples = Array.new
+  until counter == limit
+    if (counter % 3 == 0) || (counter % 5 == 0) || (counter % 9 == 0)
+      multiples.push(counter)
+    end
+    counter += 1
+  end
+  return multiples
+end
+
+def sum_multiples(limit)
+  sum = 0
+  sum_array = collect_multiples(limit)
+  sum_array.each do |multiple|
+    sum += multiple
+  end
+  return sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,32 @@
 # Enter your object-oriented solution here!
+class Multiples
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def limit
+    @limit
+  end
+
+  def collect_multiples
+    multiples = Array.new
+    counter = 1
+    until counter == limit
+      if (counter % 3 == 0) || (counter % 5 == 0) || (counter % 9 == 0)
+        multiples.push(counter)
+      end
+      counter += 1
+    end
+    return multiples
+  end
+
+  def sum_multiples
+    sum = 0
+    sum_array = collect_multiples
+    sum_array.each do |multiple|
+      sum += multiple
+    end
+    return sum
+  end
+
+end
