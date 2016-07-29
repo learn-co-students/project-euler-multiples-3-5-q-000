2807768d43f911e1270ab8f9fffea05ed640f676
Eoghan Leddy <eleddy521@gmail.com>
2016-07-29 04:24:53 +0000

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,25 @@
 # Enter your procedural solution here!
+
+
+def collect_multiples(limit)
+  multiple_container = Array.new
+  limit-=1;
+  while(limit > 0)
+    if(limit % 3==0 || limit % 5==0)
+      multiple_container.push(limit)
+      limit -= 1
+    else
+      limit-=1
+    end
+  end
+  multiple_container.reverse
+
+end
+
+def sum_multiples(limit)
+  multiple_sum = 0;
+  multiple_adder = Array.new(collect_multiples(limit))
+  multiple_adder.each { |x| multiple_sum += x }
+  multiple_sum
+
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,30 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+  def initialize(limit)
+    @limit = limit - 1
+    @multiple_container = Array.new
+    @multiple_sum = 0
+  end
+
+  def collect_multiples
+
+    while(@limit > 0)
+      if(@limit % 3==0 || @limit % 5==0)
+        @multiple_container.unshift(@limit)
+        @limit -= 1
+      else
+        @limit-=1
+      end
+    end
+    @multiple_container
+  end
+
+  def sum_multiples
+
+    collect_multiples.each { |x| @multiple_sum += x }
+    @multiple_sum
+
+  end
+
+end
