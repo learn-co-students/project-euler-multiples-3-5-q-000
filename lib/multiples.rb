953a1285af530b2a2593e15fb6ff7d02ff832c43
Margaret <eutopia@gmail.com>
2016-08-20 21:08:40 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,15 @@
-# Enter your procedural solution here!
+def collect_multiples(number)
+	multiples = []
+	(1...number).to_a.each do |num|
+		if num % 3 == 0
+			multiples << num
+		elsif num % 5 == 0
+			multiples << num
+		end
+	end
+	multiples
+end
+
+def sum_multiples(number)
+	collect_multiples(number).inject(0) {|sum, x| sum+x}
+end
