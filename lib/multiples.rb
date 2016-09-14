ec12ab121cbec82a0812d532d01a1af852216b50
Hadeel Alharthi <hadeel.alharthi@hotmail.com>
2016-09-14 18:36:03 +0300

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,16 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+  (1...limit).select do |i|
+    if (i % 3 == 0 || i % 5 == 0)
+      i
+    end
+  end
+end
+
+def sum_multiples(limit)
+  sum = 0
+  (1...limit).each do |i|
+    (i % 3 == 0 || i % 5 == 0) ? (sum += i) : nil
+  end
+  return sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,23 @@
 # Enter your object-oriented solution here!
+class Multiples
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    (1...@limit).select do |i|
+      if (i % 3 == 0 || i % 5 == 0)
+        i
+      end
+    end
+  end
+
+  def sum_multiples
+    sum = 0
+    (1...@limit).each do |i|
+      (i % 3 == 0 || i % 5 == 0) ? (sum += i) : nil
+    end
+    return sum
+  end
+
+end
