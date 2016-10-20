f1bf8ef75715f36b3a581adcf46fe358f0f18690
Sean Koo <koo.sean@gmail.com>
2016-10-20 20:15:03 +0000

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,24 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+  counter = 1
+  multiples = []
+  while counter < limit
+    if counter % 3 == 0 || counter % 5 == 0
+      multiples.push(counter)
+    end
+    counter +=1
+  end
+  return multiples
+end
+
+def sum_multiples(limit)
+  sum = 0
+  counter = 1
+  while counter < limit
+    if counter % 3 == 0 || counter % 5 == 0
+      sum += counter
+    end
+    counter +=1
+  end
+  return sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,28 @@
 # Enter your object-oriented solution here!
+class Multiples
+  def initialize(limit)
+    @limit = limit
+  end
+  def collect_multiples
+    counter = 1
+    multiples = []
+    while counter < @limit
+      if counter % 3 == 0 || counter % 5 == 0
+        multiples.push(counter)
+      end
+      counter +=1
+    end
+    return multiples
+  end
+  def sum_multiples
+    sum = 0
+    counter = 1
+    while counter < @limit
+      if counter % 3 == 0 || counter % 5 == 0
+        sum += counter
+      end
+      counter +=1
+    end
+    return sum
+  end
+end
