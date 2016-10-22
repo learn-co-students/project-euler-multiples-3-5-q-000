97614f082a8e03cdc73b7387c78845466ca03401
ToddRizley <todd.rizley@gmail.com>
2016-10-22 16:26:08 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,18 @@
 # Enter your procedural solution here!
+
+def collect_multiples(limit)
+  counter = 1
+  arr = []
+  while counter < limit do
+    if counter%5 == 0 || counter%3 == 0
+      arr.push(counter)
+    end
+    counter+= 1
+  end
+  arr
+end
+
+def sum_multiples(limit)
+  arr = collect_multiples(limit)
+  arr.inject { |sum, n| sum + n } 
+end 
