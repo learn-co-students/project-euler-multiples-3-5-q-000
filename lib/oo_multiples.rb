f15b3fbcb837f88a183686f7769f2004db938ed4
Samuel Lair <lair001@gmail.com>
2016-12-13 21:17:23 -0500

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,23 @@
 # Enter your object-oriented solution here!
+
+class Multiples
+
+	attr_accessor :limit
+
+	def initialize(limit)
+		self.limit = limit
+	end
+
+	def collect_multiples
+		@collect_multiples ||= (1..self.limit-1).to_a.keep_if do |multiple_candidate|
+			multiple_candidate % 3 == 0 || multiple_candidate % 5 == 0
+		end
+	end
+
+	def sum_multiples
+
+		@sum_multiples ||= collect_multiples.reduce { |sum, multiple| sum + multiple }
+
+	end
+
+end
