af644d85f0727ca6c4881c7251f3e2467accb19a
dustinlikens <dustinlikens@icloud.com>
2016-12-31 23:23:45 -0800

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,10 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+  Array(1...limit).select do |int|
+    int % 3 == 0 || int % 5 == 0
+  end
+end
+
+def sum_multiples(limit)
+  collect_multiples(limit).inject(0, &:+)
+end
