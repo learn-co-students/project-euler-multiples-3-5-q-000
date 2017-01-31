466431f229a9b50fb4462ade652d04f0c218b634
shoko furushita <shktkt219@gmail.com>
2017-01-31 01:07:08 -0500

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,19 @@
 # Enter your object-oriented solution here!
+class Multiples
+  attr_reader :limit, :start, :multiples
+
+  def initialize(limit)
+    @limit = limit
+    @start = 1
+    @multiples = collect_multiples
+  end
+
+  def collect_multiples
+    (1...limit).to_a.reject {|num| num unless num % 3 == 0 || num % 5 == 0}
+  end
+
+  def sum_multiples
+    collect_multiples.inject(:+)
+  end
+
+end
