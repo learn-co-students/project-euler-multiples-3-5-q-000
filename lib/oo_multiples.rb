214e7dc9c29fc0ad412b91ffb1fc091c192abc25
shoko furushita <shktkt219@gmail.com>
2017-02-02 09:55:46 -0500

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,19 +1,17 @@
 # Enter your object-oriented solution here!
 class Multiples
-  attr_reader :limit, :start, :multiples
+  attr_reader :limit, :multiples
 
   def initialize(limit)
     @limit = limit
-    @start = 1
     @multiples = collect_multiples
   end
 
   def collect_multiples
-    (1...limit).to_a.reject {|num| num unless num % 3 == 0 || num % 5 == 0}
+    (1...limit).to_a.reject {|num| num unless num % 3 == 0 || num % 5 == 0 }
   end
 
   def sum_multiples
-    collect_multiples.inject(:+)
+    multiples.inject(:+)
   end
-
 end
