e03a6b1a22ee3598b34165504d2e8731d59ca4f5
Mark Stein <mystycs@gmail.com>
2017-02-03 22:32:26 +0000

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,24 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    (1...@limit).select do |i|
+      i % 3 == 0 || i%5 == 0
+    end
+  end
+
+  def sum_multiples
+    total = 0
+    (1...@limit).select do |i|
+      if i % 3 == 0 || i % 5 == 0
+      total = total + i
+      end
+    end
+    return total
+  end
+
+end
