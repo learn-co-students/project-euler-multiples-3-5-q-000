692d4770d96d9e04d3cc0abc50bfb443cf33c909
SiamKing <thesiameseelectric@gmail.com>
2017-03-10 13:27:05 -0800

    ruby, its been a while. you are quite refreshing

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,10 @@
-# Enter your procedural solution here!
+def collect_multiples(limit)
+  (1...limit).select do |num|
+    num % 3 === 0 || num % 5 === 0
+  end
+end
+
+def sum_multiples(limit)
+  nums = collect_multiples(limit)
+  nums.inject(0, :+)
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,19 @@
-# Enter your object-oriented solution here!
+class Multiples
+
+  def initialize(limit)
+    raise ArgumentError if limit === nil
+    @limit = limit
+  end
+
+  def collect_multiples
+    (1...@limit).select do |num|
+      num % 3 === 0 || num % 5 === 0
+    end
+  end
+
+  def sum_multiples
+    nums = collect_multiples
+    nums.inject(0, :+)
+  end
+
+end
