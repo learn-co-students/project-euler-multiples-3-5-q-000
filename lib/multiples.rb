a0a1c0ce6d2535525c3f9388bc32b013a6887cf5
dchase87 <david.chase4@gmail.com>
2017-05-08 12:15:51 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,22 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+  array = *(1...limit)
+  match_array = []
+  array.map do |number|
+    if number % 3 == 0 || number % 5 == 0
+      match_array << number
+    end
+  end
+  match_array.uniq
+end
+
+def sum_multiples(limit)
+  array = *(1...limit)
+  match_array = []
+  array.map do |number|
+    if number % 3 == 0 || number % 5 == 0
+      match_array << number
+    end
+  end
+  match_array.uniq.inject(0, :+)
+end
