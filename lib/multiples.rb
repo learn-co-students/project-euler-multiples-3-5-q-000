f0bd1b56f19f4613c716bdffae9024e86d83e75c
Benjamin Cantlupe <bjcantlupe@gmail.com>
2017-05-20 18:22:09 +0000

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,23 @@
-# Enter your procedural solution here!
+def collect_multiples(limit)
+  arr = []
+  for num in [*3..limit-1] do
+    if num % 3 == 0 || num % 5 == 0
+      arr << num
+    end
+  end
+  arr
+end
+
+def sum_multiples(limit)
+  multiples = collect_multiples(limit)
+  sum = 0
+  multiples.each do |num|
+    sum += num
+  end
+  sum
+end
+
+# Problem: Multiples of 3 and 5
+
+# - If we list all of the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6, and 9. The sum of these multiples is 23.
+# - Find the __sum__ of all the multiples of 3 or 5 below 1000.
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,15 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+  def initialize
+
+  end
+
+  def collect_multiples
+
+  end
+
+  def sum_multiples
+
+  end
+end
