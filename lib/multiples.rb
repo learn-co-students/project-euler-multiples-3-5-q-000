1bc8d9a08ed408647a32763960ea05ed06c4f709
github username <github email address>
2017-06-21 21:39:33 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,9 @@
-# Enter your procedural solution here!
+def collect_multiples(limit)
+  (1...limit).to_a.reject { |number| number unless (number % 3).zero? || (number % 5).zero? }
+end
+
+def sum_multiples(limit)
+  collect_multiples(limit).inject(:+)
+end
+
+puts sum_multiples(1000)
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,17 @@
-# Enter your object-oriented solution here!
+class Multiples
+  attr_reader :limit, :start, :multiples
+
+  def initialize(limit)
+    @limit = limit - 1
+    @start = 1
+    @multiples = collect_multiples
+  end
+
+  def collect_multiples
+    (1..limit).to_a.reject { |number| number unless (number % 3).zero? || (number % 5).zero? }
+  end
+
+  def sum_multiples
+    multiples.inject(:+)
+  end
+end
