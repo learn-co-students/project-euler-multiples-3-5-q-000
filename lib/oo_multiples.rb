f3cd5b659bfec7f6dca68f96030dd424724aab85
Scott Bewick <bewick.scott@gmail.com>
2017-08-20 10:10:02 -0500

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,17 @@
-# Enter your object-oriented solution here!
+class Multiples
+
+  attr_accessor :limit
+
+  def initialize(limit)
+    @limit = limit
+  end  
+
+  def collect_multiples
+    (1...limit).to_a.reject { |num| num unless num % 3 == 0 || num % 5 == 0 }
+  end  
+
+  def sum_multiples
+    collect_multiples.inject(:+)
+  end  
+
+end
