a05d9afd0fb59658b793f0b8c4f88f55ebeb864f
mxdavis <md01@me.com>
2017-08-31 21:08:36 +0300

    done

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,11 +1,11 @@
 # Enter your procedural solution here!
 def collect_multiples(limit)
   array_of_numbers = *(3..limit - 1)
-  array_of_numbers.select do |num|
-    num % 3 == 0 || num % 5 == 0
-  end
+  array_of_numbers.select { |num| num % 3 == 0 || num % 5 == 0 }
 end
 
 def sum_multiples(limit)
-  
+  previous = 0
+  collect_multiples(limit).each { |num| previous += num }
+  previous
 end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,20 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+  attr_accessor :limit
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    array_of_numbers = *(3..@limit - 1)
+    array_of_numbers.select { |num| num % 3 == 0 || num % 5 == 0 }
+  end
+
+  def sum_multiples
+    previous = 0
+    collect_multiples.each { |num| previous += num }
+    previous
+  end
+end
