65791101262a75b04491c186b317f3a56171a320
mxdavis <md01@me.com>
2017-08-31 20:57:50 +0300

    create collect multiples function

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,11 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+  array_of_numbers = *(3..limit - 1)
+  array_of_numbers.select do |num|
+    num % 3 == 0 || num % 5 == 0
+  end
+end
+
+def sum_multiples(limit)
+  
+end
