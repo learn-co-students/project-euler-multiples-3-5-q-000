3c26ac21b348d55b5c7defdfa60c81492006709a
DanL12186 <danlrcn@hotmail.com>
2017-11-07 18:17:41 -0500

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,7 @@
-# Enter your procedural solution here!
+def collect_multiples(num, multiples = [])
+  (3...num).select { |n| n if n%3 == 0 || n%5 == 0}
+end
+
+def sum_multiples(num)
+  collect_multiples(num).reduce(:+)
+end
