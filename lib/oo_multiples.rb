615cde9e33e07a7bd50f2c593bfba763e6f94713
Samantha Gibson <sgibson47@gmail.com>
2017-12-16 16:57:23 +0000

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,18 @@
 # Enter your object-oriented solution here!
+
+#define a class Multiples
+#define a method #initialize that requires an argument (the  limit #)
+#define a method #collect_multiples that returns an array including all #the multiples of 3 and 5 up to the limit
+#define a method #sum_multiples that returns the sum of multiples of 3 and 5 up to the limit
+
+class Multiples
+  def initialize(limit)
+    @array = *(1...limit)
+  end
+  def collect_multiples
+    @array.select {|num| num % 3 == 0 || num % 5 ==0}
+  end
+  def sum_multiples
+    collect_multiples.reduce(:+)
+  end
+end
