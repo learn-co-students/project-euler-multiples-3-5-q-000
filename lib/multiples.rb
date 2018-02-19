229e72e7770eb117ebf4810bba7482ade702ab88
Nathan Robbert <admin@bigrocketgames.com>
2018-02-18 20:24:01 -0800

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,26 @@
 # Enter your procedural solution here!
+def collect_multiples(limit) 
+  multiplesArr = []
+  num = 1
+
+  while num < limit do
+    if num%3 == 0 || num%5 == 0
+      multiplesArr.push(num)
+    end
+    num += 1
+  end
+
+  return multiplesArr
+  
+end
+
+def sum_multiples(limit)
+  collection = collect_multiples(limit)
+  sum = 0
+
+  collection.each do |num|
+    sum += num
+  end
+
+  return sum 
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,34 @@
 # Enter your object-oriented solution here!
+class Multiples
+  attr_accessor :limit
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    multiplesArr = []
+    num = 1
+
+    while num < self.limit do
+      if num%3 == 0 || num%5 == 0
+        multiplesArr.push(num)
+      end
+      num += 1
+    end
+
+    return multiplesArr
+  end
+
+  def sum_multiples
+    collection = self.collect_multiples
+    sum = 0
+
+    collection.each do |num|
+      sum += num
+    end
+
+    return sum
+  end
+
+end
