35aab22bbe47de18f343d87c68a03a4ed4b3d355
Bob Habig <rfhab2@gmail.com>
2018-08-04 19:41:34 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,8 @@
-# Enter your procedural solution here!
+def collect_multiples(limit)
+    range_array = (1...limit).to_a
+    range_array.select {|n| n % 3 == 0 || n % 5 == 0}
+end
+
+def sum_multiples(limit)
+    (1...limit).to_a.reduce(0) { |s,n| n % 3 == 0 || n % 5 == 0 ? s += n : s }
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,15 @@
-# Enter your object-oriented solution here!
+class Multiples
+    attr_accessor :range_array
+
+    def initialize(limit)
+        @range_array = (1...limit).to_a
+    end
+
+    def collect_multiples
+        self.range_array.select { |n| n % 3 == 0 || n % 5 == 0 }
+    end
+
+    def sum_multiples
+        self.range_array.reduce(0) { |s, n| n % 3 == 0 || n % 5 == 0 ? s += n : s }
+    end
+end
