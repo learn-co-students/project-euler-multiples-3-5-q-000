cca59bb925c3a3624d10acafcedc6b7a1ad3955c
Sara Stanton <sara.mpa13@gmail.com>
2019-03-28 20:28:35 -0500

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,8 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+  num_array = Array(1...limit).select{|x| x % 3 == 0 || x % 5 == 0}
+end
+
+def sum_multiples(limit)
+   collect_multiples(limit).sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,19 @@
 # Enter your object-oriented solution here!
+
+class Multiples
+  attr_reader :limit
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    limit = self.limit
+    num_array = Array(1...limit).select{|x| x % 3 == 0 || x % 5 == 0}
+  end
+
+  def sum_multiples
+     collect_multiples.sum
+  end
+
+end
