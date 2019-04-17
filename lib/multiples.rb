4fc0227472076e4b734cd66ad95e7d1c75f6d309
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2019-04-17 02:06:14 +0000

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,21 @@
 # Enter your procedural solution here!
+def collect_multiples(num)
+    count = 1 
+    result = []
+
+    while count <= num do
+        multipleOfThree = count * 3
+        multipleOfFive = count * 5
+
+        (multipleOfThree < num) ? result.push(multipleOfThree) : nil
+        (multipleOfFive < num) ? result.push(multipleOfFive) : nil
+
+        count+=1
+    end
+
+    return result.sort_by(&:to_i).uniq
+end
+
+def sum_multiples(num)
+    return collect_multiples(num).inject(:+)
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,30 @@
 # Enter your object-oriented solution here!
+class Multiples
+    
+    def initialize(limit)
+        @limit = limit
+    end
+
+    def collect_multiples
+        
+        count = 1 
+        result = []
+
+        while count <= @limit do
+            multipleOfThree = count * 3
+            multipleOfFive = count * 5
+
+            (multipleOfThree < @limit) ? result.push(multipleOfThree) : nil
+            (multipleOfFive < @limit) ? result.push(multipleOfFive) : nil
+
+            count+=1
+        end
+
+        return result.sort_by(&:to_i).uniq
+    end
+
+    def sum_multiples
+     return self.collect_multiples.inject(:+)
+    end
+
+end
