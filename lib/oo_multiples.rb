2deed82013d2f1e939a751769f7dece91ae2a0a3
jason mortensen <zaross1@yahoo.com>
2019-10-29 05:35:07 +0000

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,36 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+
+  def initialize(max)
+    @max = max
+  end 
+
+  def collect_multiples
+  multiples = []
+  x = 3
+  while x < @max
+    if (x % 3 == 0) || ( x % 5 == 0)
+      multiples.push(x)
+    end
+    x += 1
+  end
+  return multiples
+end
+
+def sum_multiples
+  multiples = 0
+  x = 3
+  while x < @max
+    if (x % 3 == 0) || ( x % 5 == 0)
+      multiples += x
+    end
+    x += 1
+  end
+  return multiples
+end
+
+  
+  
+end
+
